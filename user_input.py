2514eb476f14e620f9bb4cc509a877584053237d
kepher99 <otienokepher9@gmail.com>
2024-02-28 15:54:22 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Tell me your name?")
+age = input("what is your age?")
+locations = input("where is your location?")
+
+print("My name is " + name  + " and I am " + age  + " years old and I'm from " + locations)
